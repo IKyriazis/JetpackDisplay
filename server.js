2706e1cc35b2f628c088ef13f5c03adafabeeb2f
ikyriazis <ikyriazis@wpi.edu>
2020-11-15 15:24:47 -0500

    blocks set, centering text

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: process.env.WS_PORT || 8001 });
+
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+
+// listen for requests :)
+const listener = app.listen(port, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+});
