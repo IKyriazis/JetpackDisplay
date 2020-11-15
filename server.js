cab4719dbb79231b4013808f56c04d26edb761ca
ikyriazis <ikyriazis@wpi.edu>
2020-11-15 16:24:20 -0500

    Added code to send ws message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // uses port 8001 to serve ws requests
 const express = require("express");
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: process.env.WS_PORT || 8001 });
+const wss = new WebSocket.Server({ port: 8001});
 
 const bodyParser = require("body-parser");
 const app = express();
@@ -11,6 +11,14 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+function sendToClients(msg) {
+    wss.clients.forEach((c) => {
+        if (c.readyState === WebSocket.OPEN) {
+            c.send(JSON.stringify(msg));
+        }
+    })
+}
 
 let port = 8000;
 if (port == null || port == "") {
